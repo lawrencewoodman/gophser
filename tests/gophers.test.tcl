ebccc77ca2ab9e67f315a4dd396363ef89e3858d
Lawrence Woodman <238718+lawrencewoodman@users.noreply.github.com>
2024-03-01 16:38:17 +0000

    Move test gopher server creation to procs in test_helpers.tcl

--- tests/gophers.test.tcl
+++ tests/gophers.test.tcl
@@ -6,59 +6,17 @@ set ThisScriptDir [file dirname [info script]]
 set RepoRootDir [file join $ThisScriptDir ..]
 
 source [file join $RepoRootDir gophers.tcl]
+source [file join $ThisScriptDir test_helpers.tcl]
 
 
 test init-1 {Check can read menu from server} \
 -setup {
-  # Create Gopher server
-  set t1 [thread::create -joinable {
-    vwait RepoRootDir
-    source [file join $RepoRootDir gophers.tcl]
-    set fd [file tempfile tmpConfigFilename "gopher_config.test"]
-    puts $fd "mount \"[file normalize $RepoRootDir]\" \"/\""
-    close $fd
-    gophers::init $tmpConfigFilename
-    vwait isRunning
-    vwait forever
-    gophers::shutdown
-  }]
-  # Use client to make a request
-  set t2 [thread::create -joinable {
-    vwait ThisScriptDir
-    source [file join $ThisScriptDir test_helpers.tcl]
-    vwait urlPath
-    set gopherData [TestHelpers::gopherGet localhost 7070 $urlPath]
-    thread::wait
-  }]
-
-  proc startServer {} {
-    global RepoRootDir
-    global t1
-    thread::send -async $t1 [list set RepoRootDir $RepoRootDir]
-    thread::send $t1 [list set isRunning 1]
-  }
-
-  proc shutdownServer {} {
-    global t1
-    thread::send -async $t1 [list set forever done]
-    thread::join $t1
-  }
-
-  proc getGopherSelector {urlPath} {
-    global ThisScriptDir
-    global t2
-    thread::send -async $t2 [list set ThisScriptDir $ThisScriptDir]
-    thread::send -async $t2 [list set urlPath $urlPath]
-    thread::send $t2 [list set gopherData] gopherData
-    thread::release $t2
-    thread::join $t2
-    return $gopherData
-  }
-  startServer
+  set configContent "mount \"[file normalize $RepoRootDir]\" \"/\""
+  set serverThread [TestHelpers::startServer $configContent]
 } -body {
-  lsort [split [getGopherSelector "/tests/"] "\n"]
+  lsort [split [TestHelpers::gopherGet localhost 7070 "/tests/"] "\n"]
 } -cleanup {
-  shutdownServer
+  TestHelpers::shutdownServer $serverThread
 } -result [list {} \
                 "0gophers.test.tcl\t/tests/gophers.test.tcl\tlocalhost\t7070" \
                 "0routing.test.tcl\t/tests/routing.test.tcl\tlocalhost\t7070" \
--- tests/test_helpers.tcl
+++ tests/test_helpers.tcl
@@ -14,3 +14,31 @@ proc TestHelpers::gopherGet {host port url} {
   catch {close $s}
   return $res
 }
+
+
+proc TestHelpers::startServer {configContent} {
+  set thisScriptDir [file dirname [info script]]
+  set repoRootDir [file join $thisScriptDir ..]
+  set t [thread::create -joinable {
+    vwait configContent
+    vwait repoRootDir
+    source [file join $repoRootDir gophers.tcl]
+    set fd [file tempfile tmpConfigFilename "gopher_config.test"]
+    puts $fd $configContent
+    close $fd
+    gophers::init $tmpConfigFilename
+    vwait isRunning
+    vwait forever
+    gophers::shutdown
+  }]
+  thread::send -async $t [list set configContent $configContent]
+  thread::send -async $t [list set repoRootDir $repoRootDir]
+  thread::send $t [list set isRunning 1]
+  return $t
+}
+
+
+proc TestHelpers::shutdownServer {serverThread} {
+  thread::send -async $serverThread [list set forever done]
+  thread::join $serverThread
+}
