af5633ef75eaba6e85bbfeddcb898cc23265b97f
Lawrence Woodman <238718+lawrencewoodman@users.noreply.github.com>
2024-03-02 14:09:45 +0000

    Add highload stress timing

---
+++ tests/stress/highload.tcl
@@ -0,0 +1,45 @@
+set ThisScriptDir [file dirname [info script]]
+set RepoRootDir [file join $ThisScriptDir .. ..]
+source [file join $ThisScriptDir .. test_helpers.tcl]
+
+
+proc stressServer {selectors} {
+  set timings {}
+  foreach selector $selectors {
+    lappend timings [time {TestHelpers::gopherGet localhost 7070 $selector} 50000]
+  }
+  return $timings
+}
+
+proc outputStat {name elapsed} {
+  set msPerConnection [scan $elapsed {%f microseconds per iteration}]
+  set msTotal [expr {$msPerConnection * 50000}]
+  set secTotal [expr {$msTotal / 1000000}]
+  set connectionsPerSec [expr {50000 / $secTotal}]
+
+  puts "$name:"
+  puts "   ms / connection: $msPerConnection"
+  puts "  conections / sec: $connectionsPerSec\n"
+}
+
+proc outputStats {selectors timings} {
+  foreach selector $selectors elapsed $timings {
+    outputStat $selector $elapsed
+  }
+}
+
+set configContent [join [list \
+  "log suppress all" \
+  "route \"/say/{word}\" sendWord" \
+  "mount \"[file normalize $RepoRootDir]\" \"/\"" \
+  "proc sendWord {sock selector args} {" \
+  "  sendText \$sock \[string map {\"%20\" \" \"} \$args\]" \
+  "}"] "\n"]
+
+set selectors {"/tests/" "/say/hello"}
+set serverThread [TestHelpers::startServer $configContent]
+set timings [stressServer $selectors]
+TestHelpers::shutdownServer $serverThread
+
+outputStats $selectors $timings
+
